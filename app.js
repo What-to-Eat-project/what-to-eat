410ea4846ef1bc853c07fe62e18f2813ec23905c
FJ400 <faheemandyjp@gmail.com>
2022-04-04 18:02:25 -0400

    updated js

--- app.js
+++ app.js
@@ -2,57 +2,71 @@
 
 const recipeApp = {};
 
-recipeApp.apiUrl ="https://www.themealdb.com/api/json/v1/1/list.php?i=list/images/media/meals/llcbn01574260722.jpg/preview";
+recipeApp.apiUrl ="https://www.themealdb.com/api/json/v1/1/search.php?s";
 
-const url = new URL(recipeApp.apiUrl)
-fetch(url)
-    .then((res) => res.json())
-    .then(data => {
-        food_list = data.meals;
-console.log(food_list);
-    food_list.forEach(food => {
-        const li = document.createElement("li");
-        const node = document.createTextNode(food.strIngredient);
+// recipeApp.categories = function(){
+//     fetch(recipeApp.apiUrl)
+//     .then((res) => res.json())
+//     .then((data) => {
+//         console.log(data)
+//     })
+// }
+
+recipeApp.getRecipe = function(query){
+    const url = new URL(recipeApp.apiUrl)
+
+    url.search = new URLSearchParams({
+        s: query
     })
-});
+
+    fetch(url)
+        .then((res) => res.json())
+        .then(function(data){
+            document.querySelector('#gallery').innerHTML = ''
+            recipeApp.displayRecipe(data.meals)
+        })
+}
+
+recipeApp.displayRecipe = function(data){
+    const recipe_list = data;
+    console.log(recipe_list);
+    // document.querySelector('#gallery').innerHTML = ''
+
+    recipe_list.forEach(recipes => {
+        const list = document.createElement("li");
+        const nodes = document.createTextNode(recipes.strMeal);
+        list.appendChild(nodes);
+        gallery.appendChild(list);
+
+        const anchor = document.createElement("a")
+        anchor.href=recipes.strSource; 
+
+
+        console.log(anchor)
+
+        const image = document.createElement("img");
+        image.classList.add("recipe-image");
+        image.src=recipes.strMealThumb;
+
+        gallery.appendChild(image);
+        gallery.appendChild(anchor);
+    })
+}
 
 const recipe = document.querySelector("#meal-type");
 recipe.addEventListener("change", function(){
-    console.log(recipe.value);
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${recipe.value}`)
-    .then((res) => res.json())
-    .then(data => {
-        recipe_list = data.meals;
-        console.log(recipe_list);
-
-        recipe_list.forEach(recipes => {
-            const list = document.createElement("li");
-            const nodes = document.createTextNode(recipes.strMeal);
-            list.appendChild(nodes);
-            gallery.appendChild(list);
-
-            const image = document.createElement("img");
-            image.classList.add("recipe-image");
-            image.src=recipes.strMealThumb;
-
-            gallery.appendChild(image);
-
-
-            
-
-})
-})
+    recipeApp.getRecipe(this.value)
 })
 
+recipeApp.getUserInput = function(){
+    document.querySelector('#meal-type').addEventListener("change", function(){
+        const category = this.value
+        recipeApp.getRecipe(category)
+    })
+}
+
+recipeApp.init = function(){
+    recipeApp.getUserInput()
+}
 // selecting ONLY one category
 
-// const menu = document.querySelectorAll("#meal-type");
-
-// menu.forEach((individual_menu) => {
-//     individual_menu.addEventListener('click', function () {
-
-//         const selectedRecipe = this.id;
-
-//         recipe_list.displayRecipe(recipe_list.recipes[selectedRecipe]);
-//     })
-// });
